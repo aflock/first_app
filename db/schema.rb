da6bef285a97508c5dc74ff331986cd7d6762195
aflock <andrewbflockhart@gmail.com>
2011-12-11 00:23:32 -0500

    posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111211040947) do
+ActiveRecord::Schema.define(:version => 20111211050831) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.string   "mood"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
